1a991cf6934a2f6045bec26cc8ce7e3d9674bda0
Duc Anh Le <ducanhdzz2706@gmail.com>
2024-05-24 21:17:23 +0700

    Fix main router wrong address in depositor and minter

--- contracts/script/interaction/SetAllowedSender.s.sol
+++ contracts/script/interaction/SetAllowedSender.s.sol
@@ -5,12 +5,13 @@ import { Script } from "forge-std/Script.sol";
 import { Parameters } from "../Parameters.sol";
 import { MainRouter } from "../../src/MainRouter.sol";
 import { TokenConfig } from "../config/TokenConfig.s.sol";
+import { CCIPBase } from "../../src/library/CCIPBase.sol";
 
 contract SetAllowedSender is Script, Parameters {
-    function setAllowedSender(address _sender) external {
+    function setAllowedSender(address _contract, address _sender) external {
         vm.startBroadcast();
-        MainRouter mainRouter = MainRouter(payable(AVALANCHE_FUJI_MAIN_ROUTER));
-        mainRouter.setAllowedSender(_sender, true);
+        CCIPBase _c = CCIPBase(_contract);
+        _c.setAllowedSender(_sender, true);
         vm.stopBroadcast();
         
     }
